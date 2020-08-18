dbeb28d3e8927726d51bbaa4a016d430c690e151
Davis Raimon <davisraimon@gmail.com>
2020-08-18 19:40:59 +0530

    frontend deployment files commit

---
+++ build/precache-manifest.662938935bbc6d45d8a8c1b7355b2dc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed86c4896bba25555a48156d714d8628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295d1c2da864ead8f328",
+    "url": "/static/css/2.7f8d00e9.chunk.css"
+  },
+  {
+    "revision": "b21f4dfe673d9649c79b",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "295d1c2da864ead8f328",
+    "url": "/static/js/2.0626ff1d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.0626ff1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21f4dfe673d9649c79b",
+    "url": "/static/js/main.2c131d26.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "fbc462f7799de2c4805a6c1861b60c65",
+    "url": "/static/media/logo.fbc462f7.jpg"
+  }
+]);
